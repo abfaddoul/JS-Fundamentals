7c773d04ff393e70a24c3c33bd9fb9b9df8e8f6a
abfaddou <abdelhamid.faddoul@gmail.com>
2025-05-26 20:31:06 +0100

    4rd

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,10 +1,11 @@
 const args = process.argv.slice(2);
 const x = parseInt(args[0], 10);
 
-if (!x || x < 1) {
+if (isNaN(x) || x < 1) {
 	  console.log("Missing number of occurrences");
 } else {
 	  for (let i = 0; i < x; i++) {
 		      console.log("C is fun");
 		    }
 }
+
